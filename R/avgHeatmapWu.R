c339ade5a1a3f88a130f11bf17985ffbee498692
bdj34 <50929829+bdj34@users.noreply.github.com>
2021-05-25 13:53:23 -0400

    Update avgHeatmapWu.R

--- R/avgHeatmapWu.R
+++ R/avgHeatmapWu.R
@@ -7,6 +7,7 @@ library(BiocNeighbors)
 library(uwot)
 library(data.table)
 library(matrixStats)
+library(FactoClass)
 
 #Perform a pseudo bulk analysis of the samples, then run UMAP
 #and hierarchical clustering
@@ -14,84 +15,75 @@ library(matrixStats)
 #Clear workspace
 rm(list=ls())
 
-#Set file directory to pull from and open seurat object 
-## with all the samples (already log-normalized)
-filedirOpen <- '/Users/4472241/scCode/correctedFullIntegration/'
-allSeurat <- readRDS(paste0(filedirOpen, 'integratedObject_Louvainclustering_11162020.rds'))
+#Load the processed seurat object
+dirOpen <- '/Users/4472241/scCode/redoEntirePipeline_05-05-2021/standard_pipeline_withHarmony/'
+allSeurat <- readRDS(paste0(dirOpen, 'seuratObj_05-11-2021_postStandardPipeline_withHarmony_allSamples_39+8.rds'))
 
-#Import gene list and keep first 15 genes from each type
-WuGeneList <- read.csv('/Users/4472241/scCode/geneListsProgenitorCellState_Wu2020BloodAdvances.csv', header = T, skip = 2)[2:101,]
+
+#Import gene list and keep first 60 genes from each type
+WuGeneList <- read.csv('/Users/4472241/scCode/reRunScores_seurat/Wu_Signatures.csv', header = T, skip = 2)[2:101,]
 row.names(WuGeneList) <- 1:100
-genes <- c(WuGeneList$GMP[1:30], WuGeneList$MEP[1:30], WuGeneList$HSC[1:30])
+genes <- c(WuGeneList$HSC[1:60], WuGeneList$GMP[1:60], 'CD38', WuGeneList$MEP[1:60])
+
+allSeurat <- ScaleData(allSeurat, features=genes)
 
 #Split by sample ("Identity")
-all.list <- SplitObject(allSeurat, split.by = "Identity")
+all.list <- SplitObject(allSeurat, split.by = "orig.ident")
+names(all.list)[4] <- "Zheng"
 
 #Pre-allocate matrix: rows are samples, columns are genes (avg. gene expression)
-bulkMatrix <- matrix(nrow = 46, ncol = 90)
+bulkMatrix <- matrix(nrow = 47, ncol = 181)
 
 #Fill bulkMatrix using scaled average expression
 count <- 1
 for (object in all.list){
-  #Get scaled data only for variable features
-  data <- object@assays[["RNA"]]@data[genes,]
+  #Get scaled data only for features in Wu signatures
+  data <- object@assays[["RNA"]]@scale.data[genes,]
   avgGeneExp <- rowMeans(data)
   avgGeneT <- t(avgGeneExp)
+  
   #Take the avg of every row (gene)
-  if (count < 8){
-    bulkMatrix[count+39,] <- avgGeneT
-  }else{
-    bulkMatrix[count-7,] <- avgGeneT
-  }
+  bulkMatrix[count,] <- avgGeneT
   count <- count + 1
 }
-
-#Define color based on treatment, condition
-hmaList <- data.matrix(c(5, 6, 7, 17, 32, 33, 35, 37, 39)) + 7
-RuxList <- data.matrix(c(34, 38)) +7
-color <- matrix(nrow = 46, ncol = 1)
-count1 <- 1
-count2 <- 1
-for (i in 1:46){
-  
-  if (i > 7){
-    color[i-7,] <- 'CMML'
-  } else{
-    color[i+39,] <- 'Normal'
-  }
-  
-  #Check for hma and rux
-  if (count1 < 10){
-    if (i == round(hmaList[count1])){
-      color[i-7,] <- 'HMA'
-      count1 <- count1 + 1
-    }
-  }
-  if (count2 < 3){
-    if (i == round(RuxList[count2])){
-      color[i-7,] <- 'Rux'
-      count2 <- count2 + 1
-    }
-  }
-  if ((i-7)==36){
-    color[i-7,] <- "Chemo"
-  }
-  if ((i-7)==5){
-    color[i-7,] <- "AML+HMA"
-  }
-}
-
+row.names(bulkMatrix) <- names(all.list)
 colnames(bulkMatrix) <- genes
+
 #Save files in this directory
 filedir <- "/Users/4472241/scCode/Paper_Figures/"
 coul <- colorRampPalette(c('red', 'white', 'blue'))
-group1 <- c(9,14,15,18,23,25,27,31,34)
-group3 <- c(1,5,10,11,19,24,28,29,30,36,38)
-group4 <- c(2,3,4,6,7,8,12,13,16,17,20,21,22,26,32,33,35,37,39)
-normals <- c(40,41,42,43,44,45,46)
+
+group1 <- c("SF100109106293", "SF12062800475", "SF14072200012", "2V001", "SF100109101914",
+            "6AD001", "SF14031800065", "SF16112300029", "SF14111400033")
+group3 <- c("LTB3966", "4K001", "SF100109111451", "SF100109110236", "SF14101000049", 
+            "SF12042500035", "SF14050700419", "SF16026800045", "SF16072200003", "SF14061300036", 
+            "SF15010200008")
+group4 <- c("LTB4121", "LTB6169", "4J003", "4Q001", "5E001", "5H001", "SF14040100158",
+            "SF14060200025", "SF13061200056", "4S001", "SF16112900158", "6AE001",    
+            "6AC001", "SF12092600014", "SF13032800016", "SF14110400108", "SF14092500135", 
+            "SF14080400065", "SF13070900171")
+normals <- c("SettyPt1", "SettyPt2", "SettyPt3", "Zheng",     "HuaPt1",   "HuaPt2",
+             "HuaPt3",   "HuaPt4" )
+orderList <- c(normals, group4, group1, group3)
+
+bulkMatrixOrdered <- bulkMatrix[orderList,]
+
+GMP.sums <- rowSums(bulkMatrixOrdered[,1:45])
+MEP.sums <- rowSums(bulkMatrixOrdered[,46:90])
+HSC.sums <- rowSums(bulkMatrixOrdered[,91:135])
+
 length(c(group1, group3, group4, normals))
-heatmap.2(t(bulkMatrix[c(group1, group3, group4, normals),]), colsep=c(9,20,37), rowsep=c(30,60),
+pdf(paste0(filedir, 'WuGeneHeatmap_updated_05-25-2021_UMAPGroups_noDendrogram_60GenesEach.pdf'), width = 9, height = 8)
+heatmap.2(t(bulkMatrixOrdered), colsep=c(8,27,36), rowsep=c(60,121),
           #sepwidth=c(10,.1),
           sepcolor="black", trace="none",
-          Rowv=F,Colv=F, scale="row", dendrogram="none",key=F, 
-          lhei = c(0.01,.5),margins=c(1,8), col = coul)
+          distfun = function(x) dist(x, method = "euclidean"),
+          hclustfun = function(x) hclust(x, method="ward.D2"),
+          labRow = F,
+          srtCol = 80, offsetCol = -.5,
+          Rowv=F,Colv=F, scale="none", dendrogram="none",key=F, 
+          lhei = c(0.0005,.025),margins=c(8,3), col = coul, 
+          colCol = c(rep("black", 8), rep("#D43030", length(group4)),
+                     rep("#33CEFF", length(group1)), rep("#7FC93C", length(group3))))
+      
+ dev.off()
