9cff03bf772c71d0ed5eb946c2eab91d49dc44a1
bdj34 <50929829+bdj34@users.noreply.github.com>
2022-05-14 07:36:32 -0700

    Update wnt_analysis.R

--- src/04_revision-analysis/WNT_signature/wnt_analysis.R
+++ src/04_revision-analysis/WNT_signature/wnt_analysis.R
@@ -1,4 +1,5 @@
 # WNT pathway figure
+library(ggplot2)
 library(Seurat)
 library(gridExtra)
 
@@ -6,38 +7,28 @@ rm(list = ls())
 
 allSeurat <- readRDS("~/scCode/allSeurat_withsingleR_withBias_03092022.rds")
 
+# Restrict to cluster 0 and 2 GMPs
 GMP_Seurat1 <- subset(allSeurat, subset = consensusSingleR == "GMP")
 GMP_Seurat <- subset(GMP_Seurat1, subset = clusterResolution_0.05 %in% c("0", "2"))
 
-wnt1 <- read.table("~/scCode/wnt_signatures/wnt1_GSEA_KEGG-WNT-SIGNALING-Geneset.txt", sep = "\t", skip = 2)$V1
-wnt2 <- read.csv("~/scCode/wnt_signatures/wnt2.txt", header = F)[1,]
+wnt <- read.table("~/scCode/wnt_signatures/GSEA_KEGG-WNT-SIGNALING-Geneset.txt", sep = "\t", skip = 2)$V1
 
-wnt_1_2 <- list(wnt1, wnt2)
-GMP_Seurat <- AddModuleScore(GMP_Seurat, wnt_1_2, name = "WNT_signature", search = F)
+# Add module score to seurat object
+wnt_list <- list(wnt1)
+GMP_Seurat <- AddModuleScore(GMP_Seurat, wnt_list, name = "WNT_signature", search = F)
+# SKP1P2 not found, continue without (could not find alternative name that was in our data)
 
-pdf("~/scCode/wnt_signatures/wnt1_plot.pdf")
+pdf("~/scCode/wnt_signatures/wnt_plot.pdf")
 FeaturePlot(GMP_Seurat, features = c("WNT_signature1"), order = T, min.cutoff = 0.0) + NoAxes()
 dev.off()
 
-pdf("~/scCode/wnt_signatures/wnt2_plot.pdf")
-FeaturePlot(GMP_Seurat, features = c("WNT_signature2"), order = T, min.cutoff = 0.0) + NoAxes()
-dev.off()
-
+# Make feature plots of CTNNB1, IRF8, and the WNT score we just added
 p1 <- FeaturePlot(GMP_Seurat, features = c("CTNNB1"), order = T, min.cutoff = 0.0) + NoAxes()
 p2 <- FeaturePlot(GMP_Seurat, features = c("IRF8"), order = T, min.cutoff = 0.0) + NoAxes()
 p3 <- FeaturePlot(GMP_Seurat, features = c("WNT_signature1"), order = T, min.cutoff = 0.0) + NoAxes() + ggtitle("WNT Signature")
 
+# Plot feature plots together
 pdf("~/scCode/wnt_signatures/supp_plot.pdf", width = 12/1.5, height = 5/1.5)
-grid.arrange(p1, p2, p3, ncol = 3)
-dev.off()
-
-
-
-p1 <- FeaturePlot(GMP_Seurat, features = c("CTNNB1"), order = T, min.cutoff = 0.0, raster = T) + NoAxes()
-p2 <- FeaturePlot(GMP_Seurat, features = c("IRF8"), order = T, min.cutoff = 0.0, raster = T) + NoAxes()
-p3 <- FeaturePlot(GMP_Seurat, features = c("WNT_signature1"), order = T, min.cutoff = 0.0, raster = T) + NoAxes() + ggtitle("WNT Signature")
-
-pdf("~/scCode/wnt_signatures/supp_plot_raster.pdf", width = 12, height = 5)
 grid.arrange(p1, p2, p3, ncol = 3)
 dev.off()
 
