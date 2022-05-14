fd618227010855e4843d0d11b5e709796e3ec470
bdj34 <50929829+bdj34@users.noreply.github.com>
2022-05-14 09:08:50 -0700

    Update and rename evoFreq_mitoclone_patientF_04212022.R to evoFreq_mitoclone_patientF.R

--- src/03g_mitoClone_v2/evoFreq_fish_plots/evoFreq_mitoclone_patientF.R
+++ src/03g_mitoClone_v2/evoFreq_fish_plots/evoFreq_mitoclone_patientF.R
@@ -6,83 +6,89 @@ library(viridis)
 
 rm(list = ls())
 
-# NOTE: This has the correct phylogeny for the clones present according to mitoclone for patient D
+# NOTE: This has the correct phylogeny for the clones present according to mitoclone for patient F
 
 # Set dir to save to and from which we pull countTables
 dir <- '/Users/Brian/scCode/mitoclone/'
 saveDir <- paste0(dir, 'evoFreq_04042022_newParams/')
 importDir <- paste0(dir, 'sequential_clus_clone_03272022_newParams/')
 
-#######   CHANGE PATIENT #############
+#######   SET PATIENT #############
 currentSample <- "patientF"
-D <- read.csv(paste0(importDir, currentSample, '_cluster_and_clone_info.csv'))
+F <- read.csv(paste0(importDir, currentSample, '_cluster_and_clone_info.csv'))
 
 
 # Clone
-D.table <- table(D$Sample, D$Clone)
-D.mat <- matrix(D.table, ncol = ncol(D.table), dimnames = dimnames(D.table))
-D.wide <- as.data.frame(t(100*D.mat/rowSums(D.mat)))
+F.table <- table(F$Sample, F$Clone)
+F.mat <- matrix(F.table, ncol = ncol(F.table), dimnames = dimnames(F.table))
+F.wide <- as.data.frame(t(100*F.mat/rowSums(F.mat)))
 
-#######  CHANGE: SET INITIAL COMPOSITION (SET INITIAL TO 0 IF IT STEMS FROM ANOTHER CLONE) ##########
-D.wide <- cbind(c(.01,rep(0, dim(D.wide)[1]-1)), D.wide)
+#######  SET INITIAL COMPOSITION (set initial to 0 if a clone stems from another clone) ##########
+F.wide <- cbind(c(.01,rep(0, dim(F.wide)[1]-1)), F.wide)
 
-colnames(D.wide) <- c("", "Sample 1", "Sample 2")
+colnames(F.wide) <- c("", "Sample 1", "Sample 2")
 
 ###### NUMBER CLONES FROM 1 TO N. NOTE: THIS WILL AFFECT THE PARENTS YOU SET
-D.wide$clones <- c(1:dim(D.wide)[1])
+F.wide$clones <- c(1:dim(F.wide)[1])
 ########  CHANGE PARENTS TO FIT MITOCLONE PHYLOGENY (MAKE SURE CLONES CONSISTENT WITH ABOVE NUMBERING) #########
-D.wide$parents <- c(0, rep(1, dim(D.wide)[1] - 1))
+F.wide$parents <- c(0, rep(1, dim(F.wide)[1] - 1))
 
 
 
 # Then get the frequency data. (Use ?get_evofreq for options)
-freq_frame <- get_evofreq(D.wide[,seq(1,dim(D.wide)[2]-2)], D.wide$clones, 
-                          D.wide$parents, clone_cmap = "YIGnBu")
+freq_frame <- get_evofreq(F.wide[,seq(1,dim(F.wide)[2]-2)], F.wide$clones, 
+                          F.wide$parents, clone_cmap = "YIGnBu")
 
+# Run function to set custom color scheme
 source("~/scCode/evoFreq_fixColor.R")
 freq_frame <- evoFreq_fixColor_clone(freq_frame)
 
-# Create the plot (shown on the left below)
+# Create the plot
 evo_freq_p <- plot_evofreq(freq_frame) + ylab("Relative Pop. Size") + ggtitle("Clonal Dynamics") + xlab(NULL) + 
   theme_bw() + theme(axis.text.x = element_text(face = "bold", family = "Arial", size = 12), 
                      axis.title.y = element_text(face = "bold", family = "Arial", size = 12),
                      plot.title = element_text(face = "bold", family = "Arial", size = 14))
 print(evo_freq_p)
 
-
+#table(evo_freq_p$data$clone_id, evo_freq_p$data$plot_color)
 
 
 # Do for cluster now
-D.table <- table(D$Sample, D$Clus)
-D.mat <- matrix(D.table, ncol = ncol(D.table), dimnames = dimnames(D.table))
-D.wide <- as.data.frame(t(100*D.mat/rowSums(D.mat)))
+F.table <- table(F$Sample, F$Clus)
+F.mat <- matrix(F.table, ncol = ncol(F.table), dimnames = dimnames(F.table))
+F.wide <- as.data.frame(t(100*F.mat/rowSums(F.mat)))
 
-####### CHANGE: THIS WILL DEPEND ON CLUSTREE (IF SPAWNING FROM OTHER CLUSTER, SET TO 0) ########
-D.wide <- cbind(rep(.01, dim(D.wide)[1]), D.wide)
+####### Set initial frequency ########
+F.wide <- cbind(rep(.01, dim(F.wide)[1]), F.wide)
 
-colnames(D.wide) <- c("", "Sample 1", "Sample 2")
+colnames(F.wide) <- c("", "Sample 1", "Sample 2")
 
-####### CHANGE: THIS WILL DEPEND ON THE CLUSTREE #########
-D.wide$parents <- rep(0, dim(D.wide)[1])
-D.wide$clones <- colnames(D.mat)
+####### Set parents (here all arise separately) #########
+F.wide$parents <- rep(0, dim(F.wide)[1])
+F.wide$clones <- colnames(F.mat)
 
 
 # Then get the frequency data. (Use ?get_evofreq for options)
-freq_frame <- get_evofreq(D.wide[,seq(1,dim(D.wide)[2]-2)], D.wide$clones, 
-                          D.wide$parents, clone_cmap = "jet")
+freq_frame <- get_evofreq(F.wide[,seq(1,dim(F.wide)[2]-2)], F.wide$clones, 
+                          F.wide$parents, clone_cmap = "jet")
 
+# Set color scheme
 freq_frame <- evoFreq_fixColor_clus(freq_frame)
 
-# Create the plot (shown on the left below)
+# Create the plot
 clus_freq_p <- plot_evofreq(freq_frame) + ylab("Relative Pop. Size") + ggtitle("Cluster Dynamics") + xlab(NULL) +
   theme_bw() + theme(axis.text.x = element_text(face = "bold", family = "Arial", size = 12), 
                      axis.title.y = element_text(face = "bold", family = "Arial", size = 12),
                      plot.title = element_text(face = "bold", family = "Arial", size = 14))
+
 print(clus_freq_p)
 
-#pdf(paste0(saveDir, "patient_D_clone_and_cluster.pdf"))
+#pdf(paste0(saveDir, "patient_F_clone_and_cluster.pdf"))
 
 ####### TAKE SCREENSHOT FOR NOW...PDF WASN'T WORKING FOR ME
 grid.arrange(evo_freq_p, clus_freq_p, ncol = 1)
+
+
+table(clus_freq_p[["data"]][["clone_id"]], clus_freq_p[["data"]][["plot_color"]])
 
 #dev.off()
